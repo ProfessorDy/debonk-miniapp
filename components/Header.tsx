9ca678f1b5f070a6c1dc6223eb7211931aaf623b
Destiny Adebayo <kingdeeyfx@gmail.com>
2024-09-29 08:06:19 +0100

    feat: show history icon on /swap and /referrals paths in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import Image from "next/image";
-import { FaQuestionCircle, FaBell } from "react-icons/fa";
+import { FaQuestionCircle, FaBell, FaHistory } from "react-icons/fa";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
+  // Check if pathname matches either "/swap" or "/referrals"
+  const showHistoryIcon = pathname === "/swap" || pathname === "/referrals";
+
   return (
     <header className="flex items-center justify-between bg-black p-4 shadow-md text-primary">
       <Image src="/LOGO.png" alt="App Logo" width={105} height={40} />
 
       <div className="flex items-center space-x-4">
-        <FaQuestionCircle className="text-2xl cursor-pointer" />
-
-        <FaBell className="y text-2xl cursor-pointer" />
+        {showHistoryIcon && <FaHistory />}
+        <FaQuestionCircle />
+        <FaBell />
       </div>
     </header>
   );
