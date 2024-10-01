5781686fcd5d2bb2aa50207226df24a12f4a7ed4
Destiny Adebayo <kingdeeyfx@gmail.com>
2024-10-01 07:34:00 +0100

    refactor: calculate total earnings based on number of referrals and hardcode individual referral earnings in dropdown

--- app/referrals/page.tsx
+++ app/referrals/page.tsx
@@ -7,15 +7,15 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Referrals = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [earnings] = useState(0.006);
-  const [referrals] = useState(3);
 
   const referralList = [
-    { name: "Sam Kerr", earnings: 0.002 },
-    { name: "Alex Morgan", earnings: 0.002 },
-    { name: "Megan Rapinoe", earnings: 0.002 },
-    { name: "Tobin Heath", earnings: 0.002 },
+    "Sam Kerr",
+    "Alex Morgan",
+    "Megan Rapinoe",
+    "Tobin Heath",
   ];
+  const referrals = referralList.length;
+  const totalEarnings = referrals * 0.002;
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -23,18 +23,20 @@ const Referrals = () => {
 
   return (
     <main
-      className=" pt-0 p-3 pb-20 bg-black min-h-screen  bg-repeat-y font-poppins"
+      className="pt-0 p-3 pb-20 bg-black min-h-screen bg-repeat-y font-poppins"
       style={{ backgroundImage: "url('/Rectangle.png')" }}
     >
-      <section className="mb-5 bg-[#3C3C3C3B] backdrop-blur-2xl border-[#0493CC] border-[.5px] text-primary shadow-lg rounded-x text-center font-light space-y-2 py-5 rounded-lg px-3  ">
-        <h2 className="text-xs  ">Earnings</h2>
-        <h3 className="text-3xl font-semibold text-white">{earnings} SOL</h3>
+      <section className="mb-5 bg-[#3C3C3C3B] backdrop-blur-2xl border-[#0493CC] border-[.5px] text-primary shadow-lg rounded-x text-center font-light space-y-2 py-5 rounded-lg px-3">
+        <h2 className="text-xs">Earnings</h2>
+        <h3 className="text-3xl font-semibold text-white">
+          {totalEarnings} SOL
+        </h3>
         <Link
           href="#"
-          className="text-sm  flex items-center gap-1 justify-center"
+          className="text-sm flex items-center gap-1 justify-center"
         >
           View referral rules
-          <FaExternalLinkAlt className="text-[#FFD233] text-[8px] " />
+          <FaExternalLinkAlt className="text-[#FFD233] text-[8px]" />
         </Link>
       </section>
 
@@ -42,14 +44,14 @@ const Referrals = () => {
         <p className="mb-4">
           Earn 50% fees through Debonk’s Multi-Level Referral System:
         </p>
-        <ul className=" mb-4 space-y-1">
+        <ul className="mb-4 space-y-1">
           <li>• 35% for Direct Referrals,</li>
           <li>• 10% for Second-Generation Referrals,</li>
           <li>• 5% for Third-Generation Referrals.</li>
         </ul>
       </section>
 
-      <section className="mb-5 bg-[#3C3C3C3B] backdrop-blur-2xl border-[#0493CC] border-[.5px] text-primary shadow-lg rounded-x text-center font-light space-y-2 py-5 rounded-lg px-3 ">
+      <section className="mb-5 bg-[#3C3C3C3B] backdrop-blur-2xl border-[#0493CC] border-[.5px] text-primary shadow-lg rounded-x text-center font-light space-y-2 py-5 rounded-lg px-3">
         <h3 className="font-semibold text-white flex justify-between w-full mb-3">
           My Direct Referrals <span>{referrals}</span>
         </h3>
@@ -76,8 +78,8 @@ const Referrals = () => {
                   key={index}
                   className="w-full flex justify-between items-center bg-black py-2 px-4 mb-2 border border-gray-700 rounded-lg"
                 >
-                  <span className="mr-2">{referral.name}</span>
-                  <span className="text-accent">+{referral.earnings} SOL</span>
+                  <span className="mr-2">{referral}</span>
+                  <span className="text-accent">+0.002 SOL</span>
                 </div>
               ))}
             </div>
